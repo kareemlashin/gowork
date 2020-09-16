273a113a00c1fb454ac0a2fc92587f43817e2df0
kareemlashin <Kareemelsayed887@gmail.com>
2020-09-16 09:07:41 +0200

    a

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var slider = new osSlider({
+
+
+
+    // parent container
+    pNode: '.slider',
+
+    // child elements
+
+    cNode: '.slider-main li',
+
+
+    // enable autoplay
+
+    autoPlay: true,
+
+
+    speed: 3000
+
+
+});
